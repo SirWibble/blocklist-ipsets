8b0b2055ee239b89ffdbe19475b64d7daa6d5861
Costa Tsaousis <costa@tsaousis.gr>
2017-03-29 10:53:00 +0100

    Wed Mar 29 09:53:00 UTC 2017 update

--- set_file_timestamps.sh
+++ set_file_timestamps.sh
@@ -381,7 +381,7 @@
 [ -f 'dragon_http.netset' ] && /usr/bin/touch --date=@1463536803 'dragon_http.netset'
 [ -f 'dragon_sshpauth.netset' ] && /usr/bin/touch --date=@1463583841 'dragon_sshpauth.netset'
 [ -f 'dragon_vncprobe.netset' ] && /usr/bin/touch --date=@1463583841 'dragon_vncprobe.netset'
-[ -f 'dronebl_anonymizers.netset' ] && /usr/bin/touch --date=@1490780709 'dronebl_anonymizers.netset'
+[ -f 'dronebl_anonymizers.netset' ] && /usr/bin/touch --date=@1490781011 'dronebl_anonymizers.netset'
 [ -f 'dronebl_auto_botnets.netset' ] && /usr/bin/touch --date=@1490770508 'dronebl_auto_botnets.netset'
 [ -f 'dronebl_autorooting_worms.netset' ] && /usr/bin/touch --date=@1483876208 'dronebl_autorooting_worms.netset'
 [ -f 'dronebl_compromised.netset' ] && /usr/bin/touch --date=@1489035908 'dronebl_compromised.netset'
